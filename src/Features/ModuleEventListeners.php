b6611fa3fb3032d43f169a547d3a6a3bb6dda5cb
Chris Cynarski <krzysztof@cynarski.net>
2019-09-05 17:36:20 +0100

    Allow Multiple Listeners per Event

--- src/Features/ModuleEventListeners.php
+++ src/Features/ModuleEventListeners.php
@@ -11,13 +11,25 @@ trait ModuleEventListeners
     /**
      * Returns an array of events mapped to event listeners.
      * Event class as a key and listener class as a value.
+     * For multiple listeners per event make value an array.
+     *
      * @return array
      */
     abstract public function eventListeners(): array;
 
     public function bootModuleEventListenersFeature()
     {
-        foreach($this->eventListeners() as $event=>$listener)
-            Event::listen($event, $listener);
+        foreach ($this->eventListeners() as $event => $listener) {
+
+            if (!is_array($listener)) {
+                Event::listen($event, $listener);
+                continue;
+            }
+
+            foreach ($listener as $l) {
+                Event::listen($event, $l);
+            }
+        }
+
     }
 }
