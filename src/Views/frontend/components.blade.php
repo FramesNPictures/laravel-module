e2765e902004025e79248c43c9d5d7fdbf90e8a8
Chris Cynarski <krzysztof@cynarski.net>
2019-09-17 11:04:02 +0100

    Fixing adding components from repository

--- src/Views/frontend/components.blade.php
+++ src/Views/frontend/components.blade.php
@@ -1,3 +1,7 @@
 @foreach($definition->getVueComponents() as $key => $path)
+@if(strpos($path,'/'))
 Vue.component('{{ $key }}', require('{{ $definition->relative($path) }}').default);
+@else
+Vue.component('{{ $key }}', require('{{ $path }}').default);
+@endif
 @endforeach
