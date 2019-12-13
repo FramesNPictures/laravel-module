c8a4b8f1ef9cdef7f5d99ec1ad5c40d45e780a90
Chris Cynarski <krzysztof@cynarski.net>
2019-12-13 10:24:50 +0000

    Bring back the BLADE!!!

---
+++ src/Views/frontend/javascript.blade.php
@@ -0,0 +1,26 @@
+// Javascript module for "{{ $definition->getHandle() }}"
+
+function {{ $definition->getModuleMethodName('load','javaScript') }}() {
+@if(count($definition->getJs()))
+@foreach($definition->getJs() as $path)
+    require('{{ $definition->relative($path) }}');
+@endforeach
+@else
+    // No Javascript Included
+@endif
+}
+
+function {{ $definition->getModuleMethodName('load','vueComponents') }}(vue) {
+@if(count($definition->getVueComponents()))
+@foreach($definition->getVueComponents() as $key => $path)
+@if(strpos($path,'/') !== FALSE)
+    vue.component('{{ $key }}', require('{{ $definition->relative($path) }}').default);
+@else
+    vue.component('{{ $key }}', require('{{ $path }}').default);
+@endif
+@endforeach
+@else
+    // No Vue Components
+@endif
+}
+
