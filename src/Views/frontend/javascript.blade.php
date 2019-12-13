3ee33e3d44c5da937b389ebe246e43062c11f44d
Chris Cynarski <krzysztof@cynarski.net>
2019-12-13 10:37:48 +0000

    Produce Vue Data function.

--- src/Views/frontend/javascript.blade.php
+++ src/Views/frontend/javascript.blade.php
@@ -24,3 +24,21 @@ function {{ $definition->getModuleMethodName('load','vueComponents') }}(vue) {
 @endif
 }
 
+function {{ $definition->getModuleMethodName('load','vueData') }}(vue) {
+@if(count($definition->getVueData()))
+@foreach($definition->getVueData() as $key=>$value)
+@if(is_string($value))
+    vue.{{ $key }} = '{!! $value !!}';
+@elseif(is_numeric($value))
+    vue.{{ $key }} = {!! $value !!};
+@elseif(is_array($value))
+    vue.{{ $key }} = {!! json_encode($value) !!}
+@endif
+@endforeach
+@else
+    // No Vue Data
+@endif
+}
+
+
+
